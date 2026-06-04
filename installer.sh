1c3a0548b443319e0c59875d0c21b40fa7a1e911
azroukarim <58669115+azroukarim@users.noreply.github.com>
2026-06-04 02:34:30 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -9,13 +9,13 @@ echo "          Starplex Plugin Installer Started             "
 echo "=========================================================="
 
 # 1. Remove old version completely
-echo "[1/4] Removing old version..."
+echo "[1/5] Removing old version..."
 opkg remove enigma2-plugin-extensions-starplex --force-depends > /dev/null 2>&1
 rm -rf /usr/lib/enigma2/python/Plugins/Extensions/Starplex > /dev/null 2>&1
 echo "Old version removed."
 
 # 2. Download the new version
-echo "[2/4] Downloading latest IPK from GitHub..."
+echo "[2/5] Downloading latest IPK from GitHub..."
 URL="https://github.com/azroukarim/starplex/raw/refs/heads/main/enigma2-plugin-extensions-starplex_v1.0_all.ipk"
 IPK_TMP="/tmp/starplex_install.ipk"
 
@@ -26,12 +26,17 @@ if [ ! -f $IPK_TMP ]; then
     exit 1
 fi
 
-# 3. Install the plugin
-echo "[3/4] Installing Starplex Plugin..."
+# 3. Install required dependencies
+echo "[3/5] Installing required dependencies (requests, twisted, serviceapp, players)..."
+opkg update > /dev/null 2>&1
+opkg install python3-requests python3-twisted enigma2-plugin-systemplugins-serviceapp exteplayer3 gstplayer
+
+# 4. Install the plugin
+echo "[4/5] Installing Starplex Plugin..."
 opkg install --force-reinstall --force-overwrite $IPK_TMP
 
-# 4. Clean up and restart GUI
-echo "[4/4] Cleaning up..."
+# 5. Clean up and restart GUI
+echo "[5/5] Cleaning up..."
 rm -f $IPK_TMP
 
 echo "=========================================================="
