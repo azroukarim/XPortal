a3b42a862a695e6a55f11a2e3a3995301b589e34
azroukarim <58669115+azroukarim@users.noreply.github.com>
2026-06-06 01:17:33 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -30,7 +30,7 @@ echo -e "      ${GREEN}✓ Old version removed.${NC}\n"
 
 # 2. Download the new version
 echo -e "${CYAN}[2/5]${NC} ${BOLD}Downloading latest IPK from GitHub...${NC}"
-URL="https://github.com/azroukarim/XPortal/raw/refs/heads/main/enigma2-plugin-extensions-xportal_v1.0_all.ipk"
+URL="https://github.com/azroukarim/XPortal/blob/main/enigma2-plugin-extensions-xportal_v1.1_all.ipk"
 IPK_TMP="/tmp/xportal_install.ipk"
 
 wget -q --show-progress -O $IPK_TMP $URL
